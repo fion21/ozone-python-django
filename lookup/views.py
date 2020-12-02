4d617c2ec4c72d7740eb63e7f91b8a5bdc56a52d
Fiona Lewis <31405973+fion21@users.noreply.github.com>
2020-12-02 22:14:02 +0000

    added bk col and api jumbotron

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
 def home (request):
-	return render(request, 'home.html', {})
+	import json
+	import requests
+
+	api_request =requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&API_KEY=96AD20D0-6896-49A1-8FE5-D1B5E2D53EE7")
+
+
+	try:
+		api = json.loads(api_request.content)
+
+	except Exceptoin as e:
+		api = "Error..."
+
+	return render(request, 'home.html', {'api' : api})
+
+	
+
+
+
+
 
 def about (request):
 	return render(request, 'about.html', {})
